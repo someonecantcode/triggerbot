45d0fe36f2dfbd110c642fadf614ff5e16856727
someonecantcode <s-btea@lwsd.org>
2025-05-15 19:10:31 -0700

    added larger dataset and retrained

---
+++ dataset_big2k/README.dataset.txt
@@ -0,0 +1,6 @@
+# test_1 > 2025-05-15 3:03pm
+https://universe.roboflow.com/test-qdswo/test_1-vrj7y-fa34i
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- train.py
+++ train.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 
 
 def main():
-    model = YOLO('models/yolo12n.pt')  # or yolov8s.pt, etc.
-    model.train(data="dataset/data.yaml", epochs=25, imgsz=640)
+    model = YOLO('runs/detect/train2/weights/last.pt')  # or yolov8s.pt, etc.
+    model.train(data="dataset_big2k/data.yaml", epochs=10, imgsz=640, device='cuda')
 
 if __name__ == '__main__':
     from multiprocessing import freeze_support
